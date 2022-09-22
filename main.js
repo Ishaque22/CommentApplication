a37d301a8b8ddd549b38ae14a40b2ddaa6274e7f
Ishaque22 <108622096+Ishaque22@users.noreply.github.com>
2022-09-22 12:41:34 +0000

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,8 @@ let main= document.getElementById('container-to-append')
 let replyMain= document.querySelector('.reply-append')
 let modal =document.querySelector('.background-modal')
 let userImg= document.getElementById('main-form-img')
-let cardImg =document.querySelectorAll('.card-image')
+let mainForm=document.querySelector('.form')
+let mainContainer= document.querySelector('.main-container')
 
 
 let url= './data.json';
@@ -100,3 +101,77 @@ const comment=(obj)=>{
  const toggleModal=(e)=>{
         modal.classList.toggle('show')
  }
+
+ //creating a new comment
+ const newComment=(obj)=>{
+
+    let newFormCopy=copyTemplate.cloneNode(true);
+
+    newFormCopy.querySelector('.username').textContent= `${obj.username}`
+    newFormCopy.querySelector('.date').textContent= `${obj.date}`
+    newFormCopy.querySelector('.img').innerHTML= ` <img src="${obj.image}" alt="">  `
+    newFormCopy.querySelector('.comment-card-content').value
+   
+    let footer= newFormCopy.querySelector('.reply-button')
+    if(obj.username=='juliusomo'){
+            footer.innerHTML=`  <div class="user-delete-edit-card"> 
+              <div class="delete-edit-btn">
+                <div class="delete-btn" onclick='toggleModal(event)'>
+                  <img src="./images/icon-delete.svg" alt="">
+                  <span>DELETE</span>
+                </div>
+              </div>
+              <div class="edit-btn">
+                <img src="./images/icon-edit.svg" alt="">
+                  <span>Edit</span>
+              </div>
+          </div> 
+        `
+    }
+    mainContainer.insertBefore(newFormCopy,mainForm)
+    return newFormCopy
+}
+ 
+ 
+ const allComments= JSON.parse(localStorage.getItem("finalComm")) || []
+ 
+ const addComment=(image,username,date,score)=>{
+        allComments.push({
+            image,
+            username,
+            date,
+            score
+        });
+        localStorage.setItem("finalComm", JSON.stringify(allComments));
+
+        return {image,username,date,score}
+    
+};
+allComments.forEach(newComment)
+
+
+ mainForm.onsubmit=e=>{
+    e.preventDefault();
+    console.log('submit has been triggered')
+    
+
+
+    fetch(url)
+    .then((res)=>res.json())
+    .then((data)=>{
+      let date =new Date().toLocaleDateString()
+
+    const newComments= addComment(
+        data.currentUser.image.png,
+        data.currentUser.username,
+        date,
+        0
+    )
+
+    let test= document.createElement('div')
+    test.innerHTML=`hello world`
+
+   
+    mainContainer.insertBefore(newComment(newComments),mainForm)
+}
+    )}
