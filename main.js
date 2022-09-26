f64b2c4b8e50d2a125e8ec18b8637adce5666ab8
Ishaque22 <108622096+Ishaque22@users.noreply.github.com>
2022-09-26 00:54:03 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 let template=document.getElementById('container-template').content;
 let copyTemplate=document.importNode(template,true);
-let main= document.getElementById('container-to-append')
-let replyMain= document.querySelector('.reply-append')
+let main= document.getElementById('comment-container')
+let mainContainer=document.querySelector('.container')
+let mainForm=document.querySelector('.main-form')
+let mainFormText=document.querySelector('.main-form-text-area')
+let userImg= document.getElementById('form-img')
 let modal =document.querySelector('.background-modal')
-let userImg= document.getElementById('main-form-img')
-let mainForm=document.querySelector('.form')
-let mainContainer= document.querySelector('.main-container')
-let mainFormText= document.querySelector('.form-text-area')
+
+
+
+
 
 let url= './data.json';
  fetch(url)
@@ -15,8 +18,6 @@ let url= './data.json';
     userImg.src=`${data.currentUser.image.webp }`
 })
 
-
-console.log(copyTemplate)
 
 
 fetch(url)
@@ -30,110 +31,128 @@ fetch(url)
             content:data.content
 
         }
-        comment(obj);
 
+        comments(obj);
         data.replies.forEach((data)=>{
-            let obj={
-                name:data.user.username,
-                date:data.createdAt,
-                image:data.user.image.png,
-                content:data.content
-            }
-            reply(obj);
-        })
+          let obj={
+              name:data.user.username,
+              date:data.createdAt,
+              image:data.user.image.png,
+              content:data.content
+          }
+          console.log(obj)
+         replyAppend.append(reply(obj))
+         
+      })
+
        
-    })     
+      })
+    })
+
+let cloneCopy;
+let replyAppend;
+let replyBtnForm;
+function comments(obj){
+  cloneCopy=copyTemplate.cloneNode(true)
  
-})
+  cloneCopy.querySelector('.username').textContent= `${obj.name}`
+  cloneCopy.querySelector('.date').textContent= `${obj.date}`
+  cloneCopy.querySelector('.comment-img').innerHTML= ` <img src="${obj.image}" alt="">  `
+  cloneCopy.querySelector('.comment-card-content').textContent= `${obj.content}`
+ replyAppend=cloneCopy.querySelector('.reply-card')
 
+ let replyForm=cloneCopy.getElementById('comment-reply-card');
+ let btn=cloneCopy.querySelector('.reply')
 
-const comment=(obj)=>{
-    let newCopy=copyTemplate.cloneNode(true);
-    newCopy.querySelector('.username').textContent= `${obj.name}`
-    newCopy.querySelector('.date').textContent= `${obj.date}`
-    newCopy.querySelector('.img').innerHTML= ` <img src="${obj.image}" alt="">  `
-    newCopy.querySelector('.comment-card-content').textContent= `${obj.content}`
+ btn.addEventListener('click',(e)=>{
+  replyForm.classList.toggle('show')
+}) 
 
-    let replyForm=newCopy.getElementById('reply-form');
+  main.append(cloneCopy)   
 
-    let btn=newCopy.querySelector('.reply-button')
-    btn.addEventListener('click',(e)=>{
-       replyForm.classList.toggle('show')
-    }) 
-    main.append(newCopy)   
-}
-
-
-    const reply=obj=>{
-        let newReplyCopy=copyTemplate.cloneNode(true);
-    newReplyCopy.querySelector('.username').textContent= `${obj.name}`
-    newReplyCopy.querySelector('.date').textContent= `${obj.date}`
-    newReplyCopy.querySelector('.img').innerHTML= ` <img src="${obj.image}" alt="">  `
-    newReplyCopy.querySelector('.comment-card-content').textContent= `${obj.content}`
-    let footer= newReplyCopy.querySelector('.reply-button')
-
-    let replyForm=newReplyCopy.getElementById('reply-form');
-    if(obj.name=='juliusomo'){
-            footer.innerHTML=`  <div class="user-delete-edit-card"> 
-              <div class="delete-edit-btn">
-                <div class="delete-btn" onclick='toggleModal(event)'>
-                  <img src="./images/icon-delete.svg" alt="">
-                  <span>DELETE</span>
-                </div>
-              </div>
-              <div class="edit-btn">
-                <img src="./images/icon-edit.svg" alt="">
-                  <span>Edit</span>
-              </div>
-          </div> 
-        `
-    }
-    else{
-        let btn=newReplyCopy.querySelector('.reply-button')
-        btn.addEventListener('click',(e)=>{
-           replyForm.classList.toggle('show')
-        }) 
-    }    
-    replyMain.append(newReplyCopy)  
-    }
-
-
- const toggleModal=(e)=>{
-        modal.classList.toggle('show')
- }
-
- //creating a new comment
- const newComment=(obj)=>{
-
-    let newFormCopy=copyTemplate.cloneNode(true);
-
-    newFormCopy.querySelector('.username').textContent= `${obj.username}`
-    newFormCopy.querySelector('.date').textContent= `${obj.date}`
-    newFormCopy.querySelector('.img').innerHTML= ` <img src="${obj.image}" alt="">  `
-    newFormCopy.querySelector('.comment-card-content').textContent= `${obj.content}`
    
-    let footer= newFormCopy.querySelector('.reply-button')
-    if(obj.username=='juliusomo'){
-            footer.innerHTML=`  <div class="user-delete-edit-card"> 
-              <div class="delete-edit-btn">
-                <div class="delete-btn" onclick='toggleModal(event)'>
-                  <img src="./images/icon-delete.svg" alt="">
-                  <span>DELETE</span>
-                </div>
-              </div>
-              <div class="edit-btn">
-                <img src="./images/icon-edit.svg" alt="">
-                  <span>Edit</span>
-              </div>
-          </div> 
-        `
-    }
-    mainContainer.insertBefore(newFormCopy,mainForm)
-    return newFormCopy
+    mainFormText.value=''
+
+   
+    
 }
+    
+
+
+
+
+const reply=obj=>{
+  let newReplyCopy=copyTemplate.cloneNode(true);
+newReplyCopy.querySelector('.username').textContent= `${obj.name}`
+newReplyCopy.querySelector('.date').textContent= `${obj.date}`
+newReplyCopy.querySelector('.comment-img').innerHTML= ` <img src="${obj.image}" alt="">  `
+newReplyCopy.querySelector('.comment-card-content').textContent= `${obj.content}`
+
+let replyForm=newReplyCopy.getElementById('comment-reply-card');
+let btn=newReplyCopy.querySelector('.reply')
+
+
+
+let footerRelpy= newReplyCopy.querySelector('.reply')
+if(obj.name=='juliusomo'){
+  footerRelpy.innerHTML=`  <div class="user-delete-edit-card"> 
+    <div class="delete-edit-btn">
+      <div class="delete-btn" onclick='toggleModal(event)'>
+        <img src="./images/icon-delete.svg" alt="">
+        <span>DELETE</span>
+      </div>
+    </div>
+    <div class="edit-btn">
+      <img src="./images/icon-edit.svg" alt="">
+        <span>Edit</span>
+    </div>
+</div> 
+`
+}
+else{
+  btn.addEventListener('click',(e)=>{
+     replyForm.classList.toggle('show')
+  }) 
+}    
+
+return newReplyCopy
+
+
+}
+
+
+//creating a new comment
+const newComment=(obj)=>{
+
+  let newFormCopy=copyTemplate.cloneNode(true);
+
+  newFormCopy.querySelector('.username').textContent= `${obj.username}`
+  newFormCopy.querySelector('.date').textContent= `${obj.date}`
+  newFormCopy.querySelector('.comment-img').innerHTML= ` <img src="${obj.image}" alt="">  `
+  newFormCopy.querySelector('.comment-card-content').textContent= `${obj.content}`
  
- 
- const allComments= JSON.parse(localStorage.getItem("finalComm")) || []
+  let footer= newFormCopy.querySelector('.reply')
+  if(obj.username=='juliusomo'){
+          footer.innerHTML=`  <div class="user-delete-edit-card"> 
+            <div class="delete-edit-btn">
+              <div class="delete-btn" >
+                <img src="./images/icon-delete.svg" alt="">
+                <span>Delete</span>
+              </div>
+            </div>
+            <div class="edit-btn">
+              <img src="./images/icon-edit.svg" alt="">
+                <span>Edit</span>
+            </div>
+        </div> 
+      `
+  }
+  mainContainer.insertBefore(newFormCopy,mainForm)
+  return newFormCopy
+}
+
+
+const allComments= JSON.parse(localStorage.getItem("finalComm")) || []
  
  const addComment=(image,username,date,score,content)=>{
         allComments.push({
@@ -170,23 +189,16 @@ allComments.forEach(newComment)
         mainFormText.value
     )
 
-    let test= document.createElement('div')
-    test.innerHTML=`hello world`
+   
     mainFormText.value=''
 
    
     mainContainer.insertBefore(newComment(newComments),mainForm)
 }
     )}
- 
-modalBtn.addEventListener('click',(e)=>{
-    e.preventDefault
-    let target= e.target
-    console.log(target )
-   })
 
-   function deleteForm(event){
-  toggleModal(event)
-  localStorage.clear()
+
+    const toggleModal=(e)=>{
+      modal.classList.toggle('remove')
+}
     
-   }
